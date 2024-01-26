8793b3c5d14fdd21c99ed0c829df79827b6338a6
danishrashidin <65647228+danishrashidin@users.noreply.github.com>
2024-01-26 21:17:24 +0800

    chore: add shadcn card

---
+++ components/ui/card/Card.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { cn } from '~/lib/utils'
+
+const props = defineProps({
+  class: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <div :class="cn(
+    'rounded-lg border border-neutral-200 bg-white text-neutral-950 shadow-sm dark:border-neutral-800 dark:bg-neutral-950 dark:text-neutral-50 overflow-hidden',
+    props.class,
+  )
+    ">
+    <slot />
+  </div>
+</template>
