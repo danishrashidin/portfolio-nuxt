7a273a6069eb10c939f01d424c3ec643a81fe8ee
danishrashidin <65647228+danishrashidin@users.noreply.github.com>
2024-01-31 22:12:19 +0800

    fix: project grid skeleton style

--- components/project/Grid.vue
+++ components/project/Grid.vue
@@ -9,7 +9,7 @@ defineProps<Props>()
 </script>
 
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <ProjectCardSkeleton v-if="loading" v-for="i in 3" class="" />
         <ProjectCard v-else-if="projects" v-for="project in projects" :key="project.title" :project="project" />
     </div>
